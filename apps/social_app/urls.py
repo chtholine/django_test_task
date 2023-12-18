effe7b1b1b464b19147483df4b99d58fd9f85383
chtholine <oleg123qwe@gmail.com>
2023-12-18 23:11:23 +0200

    jwt endpoints

--- apps/social_app/urls.py
+++ apps/social_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, UserActivityViewSet, LogoutAPIView, SignUpAPIView, LoginAPIView, UserViewSet
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
+from .views import PostViewSet, LogoutAPIView, SignUpAPIView, LoginAPIView, UserViewSet
+from .token_views import MyTokenObtainPairView
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'users', UserViewSet)
@@ -12,4 +14,7 @@ urlpatterns = [
     path('logout/', LogoutAPIView.as_view(), name='login'),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
