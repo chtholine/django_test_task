500f908a76c8f5a5de41e9d7f3015ed0b446aefc
chtholine <oleg123qwe@gmail.com>
2023-12-18 23:10:54 +0200

    testing bot

--- bot.py
+++ bot.py
@@ -1,112 +1,96 @@
-import json
-import random
 import requests
-
-BASE_URL = 'http://127.0.0.1:8000/api/v1/'  # Replace with your Django app's API base URL
-
-
-def read_config(config_file):
-    with open(config_file, 'r') as file:
-        config = json.load(file)
-    return config
+from random import randint
+import json
 
 
-def signup_users(number_of_users):
-    users = []
-    for _ in range(number_of_users):
-        username = f'user_{random.randint(1, 1000)}'
-        email = f'{username}@example.com'
-        password = 'password123'  # You may want to generate a random password
-
-        # Signup
-        signup_response = requests.post(
-            f'{BASE_URL}signup/',
-            data={'username': username, 'email': email, 'password': password}
-        )
-
-        if signup_response.status_code == 201:
-            # Login to obtain JWT token
-            login_response = requests.post(
-                f'{BASE_URL}login/',
-                data={'username': username, 'password': password}
-            )
-
-            if login_response.status_code == 200:
-                # Retrieve user details after login
-                user_id = get_user_id(login_response.json()['access'])
-                if user_id is not None:
-                    user_data = {
-                        'id': user_id,
-                        'username': username,
-                        'token': login_response.json()['access'],
-                    }
-                    users.append(user_data)
-                else:
-                    print(f'Error retrieving user details after login.')
-            else:
-                print(f'Error logging in after signup: {login_response.text}')
-        else:
-            print(f'Error creating user: {signup_response.text}')
-
-    return users
-
-
-def get_user_id(token):
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(f'{BASE_URL}users/', headers=headers)
-
+def get_access_token(username, password):
+    token_url = f'{base_url}token/'
+    data = {'username': username, 'password': password}
+    response = requests.post(token_url, data=data)
     if response.status_code == 200:
-        return response.json()['id']
+        return response.json().get('access')
+
+
+def refresh_access_token(refresh_token):
+    refresh_url = f'{base_url}token/refresh/'
+    data = {'refresh': refresh_token}
+    response = requests.post(refresh_url, data=data)
+    if response.status_code == 200:
+        return response.json().get('access')
+
+
+def bot():
+    # Sign up users
+    for _ in range(config['number_of_users']):
+        username = f'sarrahh{randint(1, 1000)}'
+        password = 'password'
+        user_data = {
+            "username": username,
+            "email": f"{username}@email.com",
+            "password": password,
+        }
+        response = requests.post(f'{base_url}signup/', data=user_data)
+        if response.status_code == 201:
+            print(f"User '{username}' created successfully.")
+        else:
+            print(f"Failed to create User '{username}'.")
+
+        auth_data = {
+            "username": username,
+            "password": password,
+        }
+
+        access_token = get_access_token(username, password)
+        headers = {'Authorization': f'Bearer {access_token}'}
+
+        # login
+        auth_response = requests.post(f'{base_url}login/', headers=headers, data=auth_data)
+        if auth_response.status_code == 200:
+            # create and like posts function
+            create_posts(username, headers)
+            get_and_like_posts(username, headers)
+        else:
+            print(f"Failed to authenticate User {username}.")
+
+
+def create_posts(username, headers):
+    for _ in range(randint(1, config['max_posts_per_user'])):
+        post_name = f"Post #{randint(100, 999)}"
+        post_data = {
+            "title": f"{post_name} by {username}",
+            "content": "Post content",
+            # Add any other required fields here
+        }
+        response = requests.post(f'{base_url}posts/', headers=headers, json=post_data)
+        if response.status_code == 201:
+            print(f"{post_name} by {username} created successfully.")
+        else:
+            print(f"Failed creating {post_name}")
+
+
+def get_and_like_posts(username, headers):
+    response = requests.get(f'{base_url}posts/', headers=headers)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            posts = data.get('results', [])
+            post_ids = [post['id'] for post in posts]
+            for j in range(randint(1, min(config['max_likes_per_user'], len(post_ids)))):
+                post_id = post_ids[j]
+                like_data = {"post_id": post_id}
+                response = requests.post(f'http://127.0.0.1:8000/api/v1/posts/{post_id}/like/', headers=headers,
+                                         json=like_data)
+                if response.status_code == 200:
+                    print(f"{username} liked Post{post_id} successfully.")
+        except Exception as e:
+            print(f"Error processing posts response: {e}")
+            print(response.content)
     else:
-        print(f'Error retrieving user ID: {response.text}')
-        return None
-
-
-def create_posts(users, max_posts_per_user):
-    for user in users:
-        user_id = user['id']
-        token = user['token']
-        headers = {'Authorization': f'Bearer {token}'}
-
-        num_posts = random.randint(1, max_posts_per_user)
-        for _ in range(num_posts):
-            content = f'Post content by user {user_id}'
-            requests.post(
-                f'{BASE_URL}posts/',
-                headers=headers,
-                data={'user': user_id, 'content': content}
-            )
-
-
-def like_posts(users, max_likes_per_user):
-    for user in users:
-        user_id = user['id']
-        token = user['token']
-        headers = {'Authorization': f'Bearer {token}'}
-
-        num_likes = random.randint(1, max_likes_per_user)
-        posts = requests.get(f'{BASE_URL}posts/').json()
-
-        for _ in range(num_likes):
-            post_id = random.choice(posts)['id']
-            requests.post(
-                f'{BASE_URL}/api/posts/{post_id}/like/',
-                headers=headers,
-                data={'user': user_id, 'content': 'Like content'}  # Adjust payload as needed
-            )
+        print(f"Failed to retrieve posts. Status code: {response.status_code}")
 
 
 if __name__ == "__main__":
-    config = read_config('bot_config.json')  # Replace with the path to your config file
-
-    number_of_users = config['number_of_users']
-    max_posts_per_user = config['max_posts_per_user']
-    max_likes_per_user = config['max_likes_per_user']
-
-    users = signup_users(number_of_users)
-
-    if users:
-        create_posts(users, max_posts_per_user)
-        like_posts(users, max_likes_per_user)
-    else:
-        print('Signup failed for all users.')
+    base_url = "http://127.0.0.1:8000/api/v1/"
+    with open('bot_config.json', 'r') as f:
+        config = json.load(f)
+    bot()
